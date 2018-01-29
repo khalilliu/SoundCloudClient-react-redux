583dbd9fe606e6eebe7cef4fb83d100d8fa9ecaf
khalilliu <lkhaliliu@gmail.com>
2018-01-29 17:41:11 +0800

    test setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Stream from './components/Stream';
+
 const title = 'My React Webpack Babel Setup';
 
+const tracks = [
+	{
+		title: 'some track',
+	},
+	{
+		title: 'some other track',
+	},
+];
+
 ReactDOM.render(
-	<div>{ title }</div>	,	 
+	<div>
+		<Stream tracks={tracks} /> 
+	</div>,
 	document.getElementById('app'),
 );
 
